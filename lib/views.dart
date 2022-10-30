02ff0a1de223c886aad2fcd2cb49840f71123178
MahmoudAli0 <a.gebara1002@gmail.com>
2022-10-30 20:32:13 +0200

    update main file and create the notes screen

---
+++ lib/views.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class notesScreen extends StatelessWidget {
+  const notesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
